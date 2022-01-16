21c9d3d74770d6f098547976fa04ee2ba40df75f
saffi <saffi.hartal@codefresh.io>
2022-01-16 09:16:54 +0200

    lib

---
+++ ssrf-filter.js
@@ -0,0 +1,100 @@
+const http = require('http');
+const request = require('request-promise');
+const { Agent: HttpAgent } = require('http');
+const { Agent: HttpsAgent } = require('https');
+const is_ip_private = require('private-ip');
+const patchAgent = ({
+                        isPrivate = (address) => is_ip_private(address),
+                        agent,
+                    },
+) => {
+    const createConnection = agent.createConnection;
+    agent.createConnection = function (options, fn) {
+        const { host: address } = options;
+        if (isPrivate(address)) {
+            throw new Error(`private address ${address} is not allowed.`);
+        }
+
+        const client = createConnection.call(this, options, fn);
+        client.on('lookup', (err, address) => {
+            if (err || !isPrivate(address)) {
+                return;
+            }
+
+            return client.destroy(new Error(`DNS lookup of private '${client._host}' returned ${address} is not allowed.`));
+        });
+
+        return client;
+    };
+    agent.PATCHED = true;
+    return agent;
+};
+
+
+const httpAgent = patchAgent({ agent: new HttpAgent() });
+const httpsAgent = patchAgent({ agent: new HttpsAgent() });
+
+
+const getAgent = ({ url, ssrf = true, allowListDomains = [], trace = false }) => {
+    if (!ssrf) {
+        return undefined;
+    }
+    const urlObject = new URL(url);
+    const protocol = urlObject.protocol;
+    const hostname = urlObject.hostname;
+    if (allowListDomains.includes(hostname)) {
+        trace && console.log(`Allow list match: ${hostname}, in: ${allowListDomains}, ignore ssrf`);
+        return undefined;
+    }
+    if (protocol === 'https:') return httpsAgent;
+    if (protocol === 'http:') return httpAgent;
+    new Error(`Bad protocol, url must start with http/https, Got ${url}`);
+};
+
+async function httpSsrfGet({ url, trace = true, ssrf = false, allowListDomains = [] }) {
+    trace && console.log(`Calling ${url} ssrf:${ssrf} allowListDomains:${allowListDomains}`);
+    const options = {
+        agent: getAgent({ url, ssrf, allowListDomains, trace })
+    }
+    const waitfor = new Promise((resolve, reject) => {
+        let data = [];
+        trace && console.log(`http.get ${url},  ${JSON.stringify(options)}`);
+        http.get(url, options, res => {
+            const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+            trace && console.log('Status Code:', res.statusCode);
+            trace && console.log('Date in Response header:', headerDate);
+
+            res.on('data', chunk => {
+                data.push(chunk);
+            });
+
+            res.on('end', () => {
+                trace && console.log('Response ended: ');
+                resolve({ data: data.join(''), statusCode: res.statusCode });
+            });
+        })
+            .on('error', err => {
+                trace && console.log('Error: ', err.message);
+                reject(err);
+            });
+    });
+    const result = await waitfor;
+    trace && console.log(`Called ${url} return ${JSON.stringify(result)}`);
+}
+
+async function requestSsrfGet({ url, trace = true, ssrf = true, allowListDomains = [] }) {
+    trace && console.log(`requestGet Calling ${url} ssrf:${ssrf} allowListDomains:${allowListDomains}`);
+    const options = {
+        agent: getAgent({ url, ssrf, allowListDomains, trace })
+    }
+    try {
+        const result = await request(url, options);
+        trace && console.log(`requestGet Called ${url} return ${JSON.stringify(result)}`);
+    } catch (err) {
+        trace && console.log('Error: ', err.message);
+        throw err;
+    }
+}
+
+
+module.exports = { getAgent, httpSsrfGet, requestSsrfGet };
---
+++ test-me.js
@@ -0,0 +1,134 @@
+const assertLib =  require('assert');
+const assert = assertLib.strict;
+const http = require('http');
+const { httpSsrfGet, requestSsrfGet } = require('./ssrf-filter');
+
+let exitcode = 0;
+const server = http.createServer(function (req, res) {
+    const url = req.url;
+
+    if (url === '/') {
+        // do a 200 response
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>Hello World!<h1>');
+        res.end();
+    } else if (url === '/google') {
+        // do a 302 redirect
+        res.writeHead(302, {
+            location: 'https://google.com',
+        });
+        res.end();
+    }else if (url === '/rprivate.com') {
+        // do a 302 redirect
+        res.writeHead(302, {
+            location: 'https://private.com',
+        });
+        res.end();
+    } else {
+        // do a 404 redirect
+        res.writeHead(404);
+        res.write('<h1>Sorry nothing found!<h1>');
+        res.end();
+    }
+});
+
+const PORT=3000
+server.listen(PORT);
+const baseurl = `http://0.0.0.0:${PORT}`;
+const google = `${baseurl}/google`;
+const url404 = `${baseurl}/notfound`;
+
+const testHttp = async () => {
+    const trace = true;
+    // fail
+    // await httpSsrfGet({ trace, url: `http://private.com:${PORT}`, ssrf: true });
+
+    await httpSsrfGet({ trace, url: baseurl, ssrf: false });
+    await httpSsrfGet({ trace, url: google, ssrf: false });
+    await httpSsrfGet({ trace, url: `http://private.com:${PORT}`, ssrf: false });
+    var hadFailedCnt = 0;
+    try {
+        await httpSsrfGet({ trace, url: baseurl, ssrf: true });
+    } catch(err) {
+        hadFailedCnt++;
+    }
+    assert(hadFailedCnt===1);
+    try {
+        await httpSsrfGet({ trace, url: google, ssrf: true });
+    } catch(err) {
+        hadFailedCnt++;
+    }
+    assert(hadFailedCnt===2);
+    try {
+        await httpSsrfGet({ trace, url: `http://private.com:${PORT}`, ssrf: true });
+    } catch(err) {
+        hadFailedCnt ++;
+    }
+    assert(hadFailedCnt===3);
+    await httpSsrfGet({ trace, url: `http://private.com:${PORT}`, ssrf: true, allowListDomains:['xxxx.io', 'private.com'] });
+};
+
+const testRequest = async () => {
+    const trace = true;
+    // Requires ENV to have the following export/env var.
+    // EXTERNAL_YAML_URL_WHITE_LIST=["private.com"]
+    const allowListDomains = JSON.parse(process.env.EXTERNAL_YAML_URL_WHITE_LIST);
+    await requestSsrfGet({ trace, url: `http://private.com:${PORT}`, ssrf: true, allowListDomains: allowListDomains});
+
+    await requestSsrfGet({ trace, url: baseurl, ssrf: false });
+    await requestSsrfGet({ trace, url: google, ssrf: false });
+    await requestSsrfGet({ trace, url: `http://private.com:${PORT}`, ssrf: false });
+    var hadFailedCnt = 0;
+    try {
+        await requestSsrfGet({ trace, url: baseurl, ssrf: true });
+    } catch(err) {
+        hadFailedCnt++;
+    }
+    assert(hadFailedCnt===1);
+    try {
+        await requestSsrfGet({ trace, url: google, ssrf: true });
+    } catch(err) {
+        hadFailedCnt++;
+    }
+    assert(hadFailedCnt===2);
+    try {
+        await requestSsrfGet({ trace, url: `http://private.com:${PORT}`, ssrf: true });
+    } catch(err) {
+        hadFailedCnt ++;
+    }
+    assert(hadFailedCnt===3);
+    await requestSsrfGet({ trace, url: `http://private.com:${PORT}`, ssrf: true, allowListDomains:['xxxx.io', 'private.com'] });
+    await requestSsrfGet({ trace, url: `http://private.com:${PORT}`, ssrf: true, allowListDomains:process.env.EXTERNAL_YAML_URL_WHITE_LIST});
+    try {
+        await requestSsrfGet({ trace, url: `http://rprivate.com:${PORT}`, ssrf: true });
+    } catch(err) {
+        hadFailedCnt ++;
+    }
+    assert(hadFailedCnt===4);
+    await requestSsrfGet({ trace, url: `http://rprivate.com:${PORT}`, ssrf: true, allowListDomains:['xxxx.io', 'rprivate.com'] });
+    try {
+        await requestSsrfGet({ trace, url: `http://rprivate.com:${PORT}`, ssrf: true, allowListDomains:['xxxx.io', 'private.com'] });
+    } catch(err) {
+        hadFailedCnt ++;
+    }
+    assert(hadFailedCnt===5);
+};
+
+
+const runtTests = async () => {
+    await testHttp();
+    await testRequest();
+}
+runtTests()
+    .then(() => {console.log(`Done`);})
+    .catch(error => {
+        console.log(`Had err ${error}`, error);
+        process.exit(1);
+    });
+
+
+
+setTimeout(
+    (msg)=>server.close(()=>{ console.log(msg)}),
+    3*1000,
+    `Server closed.`);
