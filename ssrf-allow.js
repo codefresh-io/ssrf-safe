a9a3103f593d021d6d82538d46c10f56f3f70995
saffi <saffi.hartal@codefresh.io>
2022-02-16 17:02:49 +0200

    CR-8947 remove engine/use ALLOW list/ remove rc-1/ simplify

--- ssrf-allow.js
+++ ssrf-allow.js
@@ -1,11 +1,5 @@
 const getAllowList = () => {
-    if ('EXTERNAL_YAML_URL_ALLOW_LIST' in process.env) {
-        return process.env['EXTERNAL_YAML_URL_ALLOW_LIST'];
-    }
-    if ('EXTERNAL_YAML_URL_WHITE_LIST' in process.env) {
-        return process.env['EXTERNAL_YAML_URL_WHITE_LIST'];
-    }
-    return [];
+     return JSON.parse(process.env['EXTERNAL_YAML_URL_ALLOW_LIST'] || process.env['EXTERNAL_YAML_URL_WHITE_LIST'] || '[]');
 }
 
 module.exports = { getAllowList };
