9370c4b2d366a98316164956d1c019f3da171716
saffi <saffi.hartal@codefresh.io>
2022-02-16 17:26:40 +0200

    wip

--- ssrf-allow.js
+++ ssrf-allow.js
@@ -1,5 +1,7 @@
 const getAllowList = () => {
-     return JSON.parse(process.env['EXTERNAL_YAML_URL_ALLOW_LIST'] || process.env['EXTERNAL_YAML_URL_WHITE_LIST'] || '[]');
+     const allowedArr = process.env['EXTERNAL_YAML_URL_ALLOW_LIST'] || process.env['EXTERNAL_YAML_URL_WHITE_LIST'] || '[]';
+     console.log(`Using allowed list:'${allowedArr}'.`)
+     return JSON.parse(allowedArr);
 }
 
 module.exports = { getAllowList };
